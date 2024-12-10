4af38ffad9857bceb1278d9b6365fe1b50ba8baa
lycei <gakupo.gumi@gmail.com>
2024-12-11 00:20:59 +1300

    touch fix 2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733828893|258531653';
+const CACHE_VERSION = '1733829650|1015556180';
 /** @type {string} */
 const CACHE_PREFIX = 'Kaves and Kobold-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
