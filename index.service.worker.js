ea5ce26e2824792873dcc1e258d4d50832b8c2f9
lycei <gakupo.gumi@gmail.com>
2024-12-14 10:51:39 +1300

    graphics overhaul

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733831846|3211794685';
+const CACHE_VERSION = '1734126651|1878487463';
 /** @type {string} */
 const CACHE_PREFIX = 'Kaves and Kobold-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
