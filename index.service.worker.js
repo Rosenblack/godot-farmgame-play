3fa1b7be19d8e2cc479be1c77851394f4895b227
lycei <gakupo.gumi@gmail.com>
2024-12-15 08:35:43 +1300

    sneeky update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734126651|1878487463';
+const CACHE_VERSION = '1734204925|18422606';
 /** @type {string} */
 const CACHE_PREFIX = 'Kaves and Kobold-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
