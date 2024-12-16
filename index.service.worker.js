bd97f66fc50527142689fa2e28932d1ee860e93f
lycei <gakupo.gumi@gmail.com>
2024-12-17 03:38:21 +1300

    gui fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734359394|312483851';
+const CACHE_VERSION = '1734359877|180996604';
 /** @type {string} */
 const CACHE_PREFIX = 'Kaves and Kobold-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
