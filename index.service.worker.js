005aa6eb14822b83001748839b9a3710d4780d4c
lycei <gakupo.gumi@gmail.com>
2024-12-21 07:32:05 +1300

    icon update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734580178|35316179';
+const CACHE_VERSION = '1734719520|237056101';
 /** @type {string} */
 const CACHE_PREFIX = 'Kaves and Kobold-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
