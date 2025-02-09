d9f51038debdb6773a61cf5aa5a48709943f6337
lycei <gakupo.gumi@gmail.com>
2025-02-10 03:00:34 +1300

    lil playable nice-ity

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734719520|237056101';
+const CACHE_VERSION = '1739109594|138614357';
 /** @type {string} */
 const CACHE_PREFIX = 'Kaves and Kobold-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
